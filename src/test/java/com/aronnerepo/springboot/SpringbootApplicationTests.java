1d845fe384136c856a1d1b2fb75e5a25d2be3c70
Aronne <285704982+Aronne-repo@users.noreply.github.com>
2026-05-26 15:41:12 +0200

    First commit

---
+++ src/test/java/com/aronnerepo/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aronnerepo.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
